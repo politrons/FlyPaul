72ea4b49d121cef01f549021cb3f1ab72ac2071f
politrons <politrons@gmail.com>
2022-08-22 18:58:33 +0200

    goal

--- src/main/scala/com/politrons/engine/BackgroundEngine.scala
+++ src/main/scala/com/politrons/engine/BackgroundEngine.scala
@@ -3,8 +3,12 @@ package com.politrons.engine
 import java.awt.Graphics
 import java.awt.event.{ActionEvent, ActionListener}
 import javax.swing._
+import java.awt.event.{ActionEvent, ActionListener}
+import java.awt.{Font, Graphics}
 
 class BackgroundEngine(goalEngine: GoalEngine) extends JLabel with ActionListener {
+
+  var playedTimeInMin = 1
 
   val imageIcon = new ImageIcon("src/main/resources/background.jpg")
   this.setIcon(imageIcon)
@@ -12,16 +16,20 @@ class BackgroundEngine(goalEngine: GoalEngine) extends JLabel with ActionListene
   setFrameDelay()
 
   private def setFrameDelay(): Unit = {
-    val DELAY = 60000 * 5 // 5 minutes duration level
+    val DELAY = 60000
     val timer = new Timer(DELAY, this)
     timer.start()
   }
 
   override def actionPerformed(e: ActionEvent): Unit = {
-    goalEngine.finishLevel()
+    playedTimeInMin += 1
+    if (playedTimeInMin == 5) goalEngine.finishLevel()
   }
 
   override protected def paintComponent(g: Graphics): Unit = {
     super.paintComponent(g)
+    g.setFont(new Font("Verdana", Font.CENTER_BASELINE, 12));
+    g.drawString(s"Time:$playedTimeInMin ", 50, 50)
+
   }
 }
